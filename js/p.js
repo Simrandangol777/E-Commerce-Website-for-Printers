65aa4276b11b443ca00106253590ab8e09313e2d
Simran Dangol <simran.dangol7@gmail.com>
2025-07-21 22:30:38 +0545

    Add files via upload

---
+++ js/p.js
@@ -0,0 +1,73 @@
+
+//functions to toggle image
+function toggle(){
+    let displayimage = document.getElementById('1')
+    if(displayimage.src.match('../images/a.png')){
+    displayimage.src = "../images/a1.png"
+    } else{
+    displayimage.src="../images/a.png"
+    }
+}
+
+function toggle1(){
+    let displayimage = document.getElementById('2')
+    if(displayimage.src.match('../images/b.png')){
+    displayimage.src = "../images/b1.png"
+    } else{
+    displayimage.src="../images/b.png"
+    }
+}
+
+function toggle2(){
+    let displayimage = document.getElementById('3')
+    if(displayimage.src.match('../images/c.png')){
+    displayimage.src = "../images/c1.png"
+    } else{
+    displayimage.src="../images/c.png"
+    }
+}
+
+function toggle3(){
+    let displayimage = document.getElementById('4')
+    if(displayimage.src.match('../images/d.png')){
+    displayimage.src = "../images/d1.png"
+    } else{
+    displayimage.src="../images/d.png"
+    }
+}
+
+function toggle4(){
+    let displayimage = document.getElementById('5')
+    if(displayimage.src.match('../images/e.png')){
+    displayimage.src = "../images/e1.png"
+    } else{
+    displayimage.src="../images/e.png"
+    }
+}
+
+function toggle5(){
+    let displayimage = document.getElementById('6')
+    if(displayimage.src.match('../images/f.png')){
+    displayimage.src = "../images/f1.png"
+    } else{
+    displayimage.src="../images/f.png"
+    }
+}
+
+function toggle6(){
+    let displayimage = document.getElementById('7')
+    if(displayimage.src.match('../images/g.png')){
+    displayimage.src = "../images/g1.png"
+    } else{
+    displayimage.src="../images/g.png"
+    }
+}
+
+function toggle7(){
+    let displayimage = document.getElementById('8')
+    if(displayimage.src.match('../images/h.png')){
+    displayimage.src = "../images/h1.png"
+    } else{
+    displayimage.src="../images/h.png"
+    }
+}
